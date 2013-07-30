aaa65407a367b2f62855ef1ee889c72881f4f239
SeongJae Park <sj38.park@gmail.com>
2013-07-30 22:50:32 +0900

    Add unittest case
    Currently, does not test anything.
    Test can be executed with next command.
    
    ```
    $ python -m unittest discover
    ```
    
    Or, use help message.
    
    ```
    $ python -m unittest -h
    ```
    
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

---
+++ test_wccordinate.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import unittest
+
+import wcoordinate
+
+class TestWcoordinate(unittest.TestCase):
+    def setUp(self):
+        print "test wcoordinate start..."
+
+    def tearDown(self):
+        print "tear down..."
